12cdb56dbefbf44d813d658f870e0156e410527f
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 11:08:01 -0300

    configurando payment handler

---
+++ src/Service/Payments/Payments.API/Payments/CreatePayment/CreatePaymentEndpoint.cs
@@ -0,0 +1,5 @@
+﻿namespace Payments.API.Payments.CreatePayment; 
+
+public class CreatePaymentEndpoint
+{
+}
---
+++ src/Service/Payments/Payments.API/Payments/CreatePayment/CreatePaymentHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Payments.API.Payments.CreatePayment;
+
+public record CreatePaymentCommand(decimal Amount, PaymentMethod PaymentMethod, 
+    PaymentStatus PaymentStatus, DateTime CreatedAt, DateTime PaidAt) : ICommand<CreatePaymentResult>; 
+public record CreatePaymentResult(Guid PaymentId);
+internal class CreatePaymentCommandHandler(IDocumentSession session, ILogger<CreatePaymentCommandHandler> logger)
+    : ICommandHandler<CreatePaymentCommand, CreatePaymentResult>
+{
+    public async Task<CreatePaymentResult> Handle(CreatePaymentCommand command, CancellationToken cancellationToken)
+    {
+        var payment = new PaymentModel
+        {
+            Id = Guid.NewGuid(),
+            Amount = command.Amount,
+            PaymentMethod = command.PaymentMethod,
+            PaymentStatus = command.PaymentStatus,
+            CreatedAt = command.CreatedAt,
+            PaidAt = command.PaidAt
+        };
+
+        session.Store(payment);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreatePaymentResult(payment.Id);
+    }
+}
