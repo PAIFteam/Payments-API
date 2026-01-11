774e968ddcbd45d4e1b384a92bd975c83d58963f
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 11:25:12 -0300

    finalizando get payment

--- src/Service/Payments/Payments.API/Payments/GetPayment/GetPaymentEndpoint.cs
+++ src/Service/Payments/Payments.API/Payments/GetPayment/GetPaymentEndpoint.cs
@@ -1,5 +1,21 @@
 ﻿namespace Payments.API.Payments.GetPayment;
 
-public class GetPaymentEndpoint
+//public record GetPaymentRequest(Guid PaymentId);
+public record GetPaymentResponse(IEnumerable<PaymentModel> Payment);
+public class GetPaymentEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/payments", async (ISender sender, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(new GetPaymentQuery(), cancellationToken);
+            var response = result.Adapt<GetPaymentResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetPayment")
+        .Produces<GetPaymentResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Payment")
+        .WithDescription("Get Payment");
+    }
 }
--- src/Service/Payments/Payments.API/Payments/GetPayment/GetPaymentHandler.cs
+++ src/Service/Payments/Payments.API/Payments/GetPayment/GetPaymentHandler.cs
@@ -1,5 +1,17 @@
 ﻿namespace Payments.API.Payments.GetPayment; 
 
-public class GetPaymentHandler
+public record GetPaymentQuery() : IQuery<GetPaymentResult>;
+public record GetPaymentResult(IEnumerable<PaymentModel> Payment);
+internal class GetPaymentQueryHandler(IDocumentSession session, ILogger<GetPaymentQueryHandler> logger)
+    : IQueryHandler<GetPaymentQuery, GetPaymentResult>
 {
+    public async Task<GetPaymentResult> Handle(GetPaymentQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetPaymentQueryHandler.Handle called with {@Query}", query);
+
+        var payment = await session.Query<PaymentModel>()
+            .ToListAsync(cancellationToken);
+
+        return new GetPaymentResult(payment);
+    }
 }
