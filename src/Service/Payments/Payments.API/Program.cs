ee44ead5d313645ab7876fd7829cbc5bc3b4ef60
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 10:32:09 -0300

    configurando o program

--- src/Service/Payments/Payments.API/Program.cs
+++ src/Service/Payments/Payments.API/Program.cs
@@ -1,6 +1,19 @@
+using Carter;
+using Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
 
+app.MapCarter();
 app.Run();
