1cec9b92db91fcdc2cef28c8f12826f097aa7096
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 11:40:43 -0300

    finalizando get by id

--- src/Service/Payments/Payments.API/Payments/GetPaymentById/GetPaymentByIdEndpoint.cs
+++ src/Service/Payments/Payments.API/Payments/GetPaymentById/GetPaymentByIdEndpoint.cs
@@ -1,5 +1,22 @@
 ﻿namespace Payments.API.Payments.GetPaymentById;
 
-public class GetPaymentByIdEndpoint
+//public record GetPaymentByIdRequest(Guid PaymentId);
+public record GetPaymentByIdResponse(PaymentModel Payment);
+public class GetPaymentByIdEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/payments/{paymentId:guid}", async (Guid Id, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(new GetPaymentByIdQuery(Id), cancellationToken);
+            var response = result.Adapt<GetPaymentByIdResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("GetPaymentById")
+        .Produces<GetPaymentByIdResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .WithSummary("Get Payment By Id")
+        .WithDescription("Get Payment By Id");
+    }
 }
--- src/Service/Payments/Payments.API/Payments/GetPaymentById/GetPaymentByIdHandler.cs
+++ src/Service/Payments/Payments.API/Payments/GetPaymentById/GetPaymentByIdHandler.cs
@@ -1,5 +1,21 @@
 ﻿namespace Payments.API.Payments.GetPaymentById;
 
-public class GetPaymentByIdHandler
+public record GetPaymentByIdQuery(Guid Id) : IQuery<GetPaymentByIdResult>; 
+public record GetPaymentByIdResult(PaymentModel Payment);
+internal class GetPaymentByIdQueryHandler(IDocumentSession session, ILogger<GetPaymentByIdQueryHandler> logger)
+    : IQueryHandler<GetPaymentByIdQuery, GetPaymentByIdResult>
 {
+    public async Task<GetPaymentByIdResult> Handle(GetPaymentByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling GetPaymentByIdQuery for PaymentId: {PaymentId}", query);
+
+        var payment = await session.LoadAsync<PaymentModel>(query.Id, cancellationToken);
+
+        if(payment is null)
+        {
+            throw new PaymentNotFoundException();
+        }
+
+        return new GetPaymentByIdResult(payment);
+    }
 }
