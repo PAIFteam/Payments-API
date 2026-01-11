c3e71f64b2d783046028975d8ff53ec3f95bf6d5
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 10:26:16 -0300

    criando payment model

---
+++ src/Service/Payments/Payments.API/Models/PaymentModel.cs
@@ -0,0 +1,19 @@
+﻿using Payments.API.Enums;
+
+namespace Payments.API.Models;
+
+public class PaymentModel
+{
+    public Guid Id { get; set; }
+    public decimal Amount { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+    public PaymentStatus PaymentStatus { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime PaidAt { get; set; }
+
+    public PaymentModel()
+    {
+        CreatedAt = DateTime.UtcNow;
+        PaymentStatus = PaymentStatus.Pending;
+    }
+}
