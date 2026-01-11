3c1cd0ef508d14d67461d83fc50c863baaaf69ec
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 11:14:57 -0300

    finalizando o create payment

--- src/Service/Payments/Payments.API/GlobalUsing.cs
+++ src/Service/Payments/Payments.API/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using Marten;
 global using Payments.API.Enums;
 global using BuildingBlocks.CQRS;
 global using Payments.API.Models;
+global using Mapster;
+global using MediatR;
--- src/Service/Payments/Payments.API/Payments/CreatePayment/CreatePaymentEndpoint.cs
+++ src/Service/Payments/Payments.API/Payments/CreatePayment/CreatePaymentEndpoint.cs
@@ -1,5 +1,24 @@
 ﻿namespace Payments.API.Payments.CreatePayment; 
 
-public class CreatePaymentEndpoint
+public record CreatePaymentRequest(decimal Amount, PaymentMethod PaymentMethod, 
+    PaymentStatus PaymentStatus, DateTime CreatedAt, DateTime PaidAt);
+public record CreatePaymentResponse(Guid PaymentId);
+public class CreatePaymentEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/payments", async (CreatePaymentRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreatePaymentCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreatePaymentResponse>();
+
+            return Results.Created($"/payments/{result.PaymentId}", response);
+        })
+        .WithName("CreatePayment")
+        .Produces<CreatePaymentResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Payment")
+        .WithDescription("Create Payment");
+    }
 }
