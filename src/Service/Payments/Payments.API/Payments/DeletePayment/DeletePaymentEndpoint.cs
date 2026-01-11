e86fa3e6f3b3ca5d592f34fb269122a1c7c052c4
Caio Monteiro <caiomonteiropro@gmail.com>
2026-01-11 12:03:27 -0300

    finzalizando o CRUD

---
+++ src/Service/Payments/Payments.API/Payments/DeletePayment/DeletePaymentEndpoint.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Payments.API.Payments.DeletePayment;
+
+//public record DeletePaymentRequest(Guid PaymentId);
+public record DeletePaymentResponse(bool Success);
+public class DeletePaymentEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/payments/{Id:guid}", async (Guid Id, ISender sender, CancellationToken cancellationToken) =>
+        {
+            var result = await sender.Send(new DeletePaymentCommand(Id), cancellationToken);
+            var response = result.Adapt<DeletePaymentResponse>();
+            return Results.Ok(response);
+        })
+        .WithName("DeletePayment")
+        .Produces<DeletePaymentResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Payment")
+        .WithDescription("Delete Payment");
+    }
+}
---
+++ src/Service/Payments/Payments.API/Payments/DeletePayment/DeletePaymentHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Payments.API.Payments.DeletePayment;
+
+public record DeletePaymentCommand(Guid Id) : ICommand<DeletePaymentResult>;
+public record DeletePaymentResult(bool Success);
+internal class DeletePaymentCommandHandler(IDocumentSession session, ILogger<DeletePaymentCommandHandler> logger)
+    : ICommandHandler<DeletePaymentCommand, DeletePaymentResult>
+{
+    public async Task<DeletePaymentResult> Handle(DeletePaymentCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeletePaymentCommandHandler.Handle called with {@Command}", command);
+
+        session.Delete(command.Id);
+        await session.SaveChangesAsync();
+
+        return new DeletePaymentResult(true);
+    }
+}
